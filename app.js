f5a49a4d694dcc2a624a0873498bc7e3ad4c8ab7
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-18 11:20:36 -0400

    did some things and broke some others

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-// const initLoad = async function initialLoad() {
-//     try {
-//       const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=20&breed_ids=beng&api_key=has_breeds=1");
-//       const data = await response.json();
-//       this._cat = data.results;
-//       for(let i = 0; i <=this._cat.length; i++){
-//         console.log(this._cat[i].name);
-//       }
-//       catch(error){
-//         console.error(error);
-//       }
-//     }
-//   };
+const breedSelect = document.getElementById("breedSelect");
+const infoDump = document.getElementById("infoDump");
+const progressBar = document.getElementById("progressBar");
+const getFavouritesBtn = document.getElementById("getFavouritesBtn");
+const API_KEY =
+  "live_cTBlPlitjQXr92cOiyHljrHCUgNRtTVHXbjSTprMCloO2IXWS2tGbPxB6KvYSsbA";
 
-const fetchData = async() =>{
-    try {
-        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=20&breed_ids=beng&api_key=has_breeds=1");
-        const {breeds} = await response.json();
-        console.log(breeds)
+const url = `https://api.thecatapi.com/v1/breeds`;
+let storedBreeds = [];
+
+fetch(url, {
+  headers: {
+    "x-api-key": API_KEY,
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data = data.filter((img) => img.image?.url != null);
+
+    storedBreeds = data;
+
+    for (let i = 0; i < storedBreeds.length; i++) {
+      const breed = storedBreeds[i];
+      let option = document.createElement("option");
+      if (!breed.image) continue;
+
+      option.value = i;
+      option.innerHTML = `${breed.name}`;
+      breedSelect.appendChild(option);
     }
-};
+    showBreedImage(0);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function showBreedImage(index) {
+  document.getElementById("breed_image").src = storedBreeds[index].image.url;
+
+  document.getElementById("breed_json").textContent =
+    storedBreeds[index].temperament;
+
+  document.getElementById("wiki_link").href = storedBreeds[index].wikipedia_url;
+  document.getElementById("wiki_link").innerHTML =
+    storedBreeds[index].wikipedia_url;
+}
