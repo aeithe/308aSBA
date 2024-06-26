eab0764b93e2d99af71e2425a8905af25cea5e88
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-26 10:07:47 -0400

    removed clog

--- digimoncardfactory.js
+++ digimoncardfactory.js
@@ -1,5 +1,4 @@
 export default function digimonCardFactory(digimon){
-    console.log(digimon)
     let card = document.createElement("div");
     let img = document.createElement("img");
     let cardBody = document.createElement("div");
